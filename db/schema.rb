f049af4f947a30c47b42e827d2e980f33f7c0c2f
Vinícius <contato@viniciusalonso.com>
2025-06-21 09:47:14 -0300

    Create model Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.2].define(version: 2025_06_21_124550) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
 
+  create_table "pgbench_accounts", primary_key: "aid", id: :integer, default: nil, force: :cascade do |t|
+    t.integer "bid"
+    t.integer "abalance"
+    t.string "filler", limit: 84
+  end
+
+  create_table "pgbench_branches", primary_key: "bid", id: :integer, default: nil, force: :cascade do |t|
+    t.integer "bbalance"
+    t.string "filler", limit: 88
+  end
+
+  create_table "pgbench_history", id: false, force: :cascade do |t|
+    t.integer "tid"
+    t.integer "bid"
+    t.integer "aid"
+    t.integer "delta"
+    t.datetime "mtime", precision: nil
+    t.string "filler", limit: 22
+  end
+
+  create_table "pgbench_tellers", primary_key: "tid", id: :integer, default: nil, force: :cascade do |t|
+    t.integer "bid"
+    t.integer "tbalance"
+    t.string "filler", limit: 84
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "slug"
+    t.integer "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
