baea8e5fb0d5dd464eb13553617636131728a98f
Vinícius <contato@viniciusalonso.com>
2025-06-22 15:20:50 -0300

    Fix table name

--- db/migrate/20250622125516_create_posts_with_index.rb
+++ db/migrate/20250622125516_create_posts_with_index.rb
@@ -1,6 +1,6 @@
-class CreatePostsWithIndexTable < ActiveRecord::Migration[7.2]
+class CreatePostsWithIndex < ActiveRecord::Migration[7.2]
   def change
-    create_table :posts_with_index_tables do |t|
+    create_table :posts_with_index do |t|
       t.string :title
       t.text :body
       t.string :slug
--- db/schema.rb
+++ db/schema.rb
@@ -24,13 +24,13 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_22_130238) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "posts_with_index_tables", force: :cascade do |t|
+  create_table "posts_with_index", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.string "slug"
     t.integer "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_posts_with_index_tables_on_author_id"
+    t.index ["author_id"], name: "index_posts_with_index_on_author_id"
   end
 end
