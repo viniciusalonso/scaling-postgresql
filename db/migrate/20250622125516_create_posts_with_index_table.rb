fd78b126e84584fe2a59ec8a673bd40f4a844a71
Vinícius <contato@viniciusalonso.com>
2025-06-22 09:58:24 -0300

    Create table posts with index

---
+++ db/migrate/20250622125516_create_posts_with_index_table.rb
@@ -0,0 +1,11 @@
+class CreatePostsWithIndexTable < ActiveRecord::Migration[7.2]
+  def change
+    create_table :posts_with_index_tables do |t|
+      t.string :title
+      t.text :body
+      t.string :slug
+      t.integer :author_id, index: true
+      t.timestamps
+    end
+  end
+end
